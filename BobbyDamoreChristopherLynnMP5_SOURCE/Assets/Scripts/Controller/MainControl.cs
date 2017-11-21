fdb15f0186abc004a6adbcab02a36814e1caf395
Bobby Damore <damorer@uw.edu>
2017-11-20 19:30:14 -0800

    Added the ability to select a mesh controller
    You can now select one of the mesh controllers with a left click and
    notice it turn yellow. The mSelected GameObject in MainController keeps
    track of the selected control so that we can eventually move it around
    with GUI.

---
+++ BobbyDamoreChristopherLynnMP5_SOURCE/Assets/Scripts/Controller/MainControl.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class MainControl : MonoBehaviour {
+
+	public CameraControl mCamControl;
+	public GameObject mSelected;
+    public MyMesh mMesh;
+
+	// Use this for initialization
+	void Start() {
+        Debug.Assert(mCamControl != null);
+        Debug.Assert(mMesh != null);
+        mSelected = null;
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        // Try to update the selection on every left mouse click
+        if (Input.GetMouseButtonDown(0)) 
+		{
+	        changeSelection(mCamControl.UpdateSelection());
+		}
+	}
+
+    // Only change selection when clicking on a new mesh control
+	private void changeSelection(GameObject selected)
+	{
+        if (selected != null && mMesh.IsMyControl(selected))
+        {
+            // Update last selected color back to white
+            if (mSelected != null)
+            {
+                mSelected.GetComponent<Renderer>().material.SetColor("_Color", Color.white);
+                mSelected = null;
+            }
+            // Change newly selected color to yellow
+            mSelected = selected;
+            mSelected.GetComponent<Renderer>().material.SetColor("_Color", Color.yellow);
+        }
+	}
+}
--- BobbyDamoreChristopherLynnMP5_SOURCE/Assets/Scripts/Model/LineSegment.cs
+++ BobbyDamoreChristopherLynnMP5_SOURCE/Assets/Scripts/Model/LineSegment.cs
@@ -2,14 +2,18 @@
 
 public class LineSegment : MonoBehaviour
 {
-    public void SetWidth(float v)
+	private float mWidth;
+
+	public void SetWidth(float v)
     {
         transform.localScale = new Vector3(v, transform.localScale.y, v);
+		mWidth = v;
     }
 
     public void SetEndPoints(Vector3 start, Vector3 end)
     {
         transform.up = (end - start).normalized;
         transform.localPosition = (start + end) / 2;
+		transform.localScale = new Vector3 (mWidth, (end - start).magnitude / 2.0f, mWidth);
     }
 }
